e8bd198370f1cb31bc4fcc05d86ab793d131b541
fans-PC\Jasper <shazigx@yahoo.com>
2017-02-15 13:40:26 +0800

    实现基本功能，包含文字和图片

---
+++ out/main.js
@@ -0,0 +1,83 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+window.onload = function () {
+    var canvas = document.getElementById("app");
+    var context2D = canvas.getContext("2d");
+    var stage = new DisplayObjectContainer();
+    setInterval(function () {
+        context2D.clearRect(0, 0, canvas.width, canvas.height);
+        stage.draw(context2D);
+    }, 100);
+    //文字
+    var name = new TextField();
+    name.x = 70;
+    name.y = 70;
+    name.text = "Jasper";
+    name.color = "#FFFFCC";
+    name.size = 50;
+    //图片
+    var image = document.createElement("img");
+    image.src = "avater.jpg";
+    image.onload = function () {
+        var avater = new Bitmap();
+        avater.image = image;
+        avater.width = 300;
+        avater.height = 300;
+        stage.addChild(avater);
+        stage.addChild(name);
+    };
+};
+var DisplayObject = (function () {
+    function DisplayObject() {
+        this.x = 0;
+        this.y = 0;
+    }
+    DisplayObject.prototype.draw = function (context2D) {
+    };
+    return DisplayObject;
+}());
+var DisplayObjectContainer = (function () {
+    function DisplayObjectContainer() {
+        this.array = [];
+    }
+    DisplayObjectContainer.prototype.draw = function (context2D) {
+        for (var _i = 0, _a = this.array; _i < _a.length; _i++) {
+            var drawable = _a[_i];
+            drawable.draw(context2D);
+        }
+    };
+    DisplayObjectContainer.prototype.addChild = function (displayObject) {
+        this.array.push(displayObject);
+    };
+    return DisplayObjectContainer;
+}());
+var Bitmap = (function (_super) {
+    __extends(Bitmap, _super);
+    function Bitmap() {
+        _super.apply(this, arguments);
+    }
+    Bitmap.prototype.draw = function (context2D) {
+        context2D.drawImage(this.image, this.x, this.y, this.width, this.height);
+    };
+    return Bitmap;
+}(DisplayObject));
+var TextField = (function (_super) {
+    __extends(TextField, _super);
+    function TextField() {
+        _super.apply(this, arguments);
+        this.text = "";
+        this.color = "";
+        this.size = 40;
+        this.font = "Arial";
+    }
+    TextField.prototype.draw = function (context2D) {
+        context2D.fillStyle = this.color;
+        context2D.font = this.size + "px" + " " + this.font;
+        context2D.fillText(this.text, this.x, this.y);
+    };
+    return TextField;
+}(DisplayObject));
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,113 @@
+window.onload = () => {
+
+    var canvas = document.getElementById("app") as HTMLCanvasElement;
+    var context2D = canvas.getContext("2d");
+
+
+    var stage = new DisplayObjectContainer();
+
+    setInterval(() => {
+        
+        context2D.clearRect(0, 0, canvas.width, canvas.height);
+        stage.draw(context2D);
+
+    }, 100)
+
+
+    //文字
+    var name = new TextField();
+    name.x = 70;
+    name.y = 70;
+    name.text = "Jasper";
+    name.color = "#FFFFCC";
+    name.size = 50;
+    
+    //图片
+    var image = document.createElement("img");
+    image.src = "avater.jpg";
+
+    image.onload = () => {
+
+        var avater = new Bitmap();
+        avater.image = image;
+        avater.width = 300;
+        avater.height = 300;
+
+        stage.addChild(avater);
+        stage.addChild(name);
+    }
+
+}
+
+
+interface Drawable{
+    
+    draw(context2D:CanvasRenderingContext2D);
+
+}
+
+class DisplayObject implements Drawable{
+
+    x:number = 0;
+    y:number = 0;
+    
+    draw(context2D:CanvasRenderingContext2D) {
+
+
+    }
+}
+
+class DisplayObjectContainer implements Drawable {
+    
+    array:Drawable[] = [];
+    
+    draw(context2D:CanvasRenderingContext2D) {
+
+        for (let drawable of this.array) {
+
+            drawable.draw(context2D);
+        }
+    }
+
+    addChild(displayObject:DisplayObject){
+
+        this.array.push(displayObject);
+
+    }
+}
+
+class Bitmap extends DisplayObject {
+    
+    image:HTMLImageElement;
+    width:number;
+    height:number;
+
+    draw(context2D:CanvasRenderingContext2D) {
+
+        context2D.drawImage(this.image,this.x,this.y,this.width,this.height);
+        
+    }
+}
+
+class TextField extends DisplayObject {
+    
+    text:string = "";
+    color:string = "";
+    size:number = 40;
+    font:string = "Arial";
+
+    draw(context2D:CanvasRenderingContext2D) {
+        
+        context2D.fillStyle = this.color;
+        context2D.font = this.size +"px" + " " + this.font;
+        context2D.fillText(this.text,this.x,this.y);
+    }
+
+}
+
+
+
+
+
+
+
